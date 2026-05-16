8d3d40ed47edc9096395dc8cab7ee1a0755ab0ad
Denis Kolesnichenko <dentv2017@gmail.com>
2026-05-16 20:11:10 +0300

    Add files via upload

--- schemas.py
+++ schemas.py
@@ -1,1 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+
+    class Config:
+        orm_mode = True
