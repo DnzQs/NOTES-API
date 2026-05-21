cd01b6d76973c5acb5cbe847b156467fbacb2d0f
Denis Kolesnichenko <dentv2017@gmail.com>
2026-05-21 21:35:36 +0300

    Add files via upload

--- models.py
+++ models.py
@@ -1,14 +1,42 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
-from app.db import Base
+from datetime import datetime, UTC
 
+from app.db import Base
 
 
 class User(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
+
     email = Column(String, unique=True, index=True, nullable=False)
+
     hashed_password = Column(String, nullable=False)
 
-    #  notes = relationship("Note", back_populates="user")
+    notes = relationship("Note", back_populates="owner")
+
+
+class Note(Base):
+    __tablename__ = "notes"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    title = Column(String, nullable=False)
+
+    content = Column(String, nullable=False)
+
+    created_at = Column(
+        DateTime,
+        default=lambda: datetime.now(UTC)
+    )
+
+    owner_id = Column(
+        Integer,
+        ForeignKey("users.id")
+    )
+
+    owner = relationship(
+        "User",
+        back_populates="notes"
+    )
