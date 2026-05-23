3ec8f2865cc22eff8cab91a1c464adc61bb8e270
Denis Kolesnichenko <dentv2017@gmail.com>
2026-05-23 19:01:43 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
-from app.routes import auth
-from app.db import Base, engine
 
-app = FastAPI()
+from app.db import Base, engine
+from app.routes.auth import router as auth_router
+from app.routes.notes import router as notes_router
+
 
 Base.metadata.create_all(bind=engine)
 
-app.include_router(auth.router, prefix="/auth")
+app = FastAPI()
+
+app.include_router(auth_router)
+app.include_router(notes_router)
