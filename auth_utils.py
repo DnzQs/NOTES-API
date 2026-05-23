90500413c7ea592f2f91078d57e5ddd9dae1b7e1
Denis Kolesnichenko <dentv2017@gmail.com>
2026-05-23 19:13:15 +0300

    Add files via upload

--- auth_utils.py
+++ auth_utils.py
@@ -1,36 +1,27 @@
 import os
+
 from fastapi import Depends, HTTPException
-from passlib.context import CryptContext
-from jose import jwt, JWTError
 from fastapi.security import OAuth2PasswordBearer
+
+from jose import jwt, JWTError
+
+from passlib.context import CryptContext
+
 from datetime import datetime, timedelta, UTC
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt"],deprecated="auto")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
+
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 
 if not SECRET_KEY or not ALGORITHM:
     raise ValueError("Missing JWT config in .env")
-
-def get_current_user(token: str = Depends(oauth2_scheme)):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        email = payload.get("sub")
-
-        if email is None:
-            raise HTTPException(status_code=400, detail="Invalid token")
-
-        return email
-
-    except JWTError:
-        raise HTTPException(status_code=400, detail="Invalid token")
-
-
 
 
 def hash_password(password: str):
@@ -42,8 +33,29 @@ def verify_password(plain_password, hashed_password):
 
 
 def create_access_token(data: dict):
+
     to_encode = data.copy()
+
     expire = datetime.now(UTC) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
     to_encode.update({"exp": expire})
 
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme)
+):
+
+    try:
+        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+
+        email = payload.get("sub")
+
+        if email is None:
+            raise HTTPException(status_code=401,detail="Invalid token")
+
+        return email
+
+    except JWTError:
+        raise HTTPException(status_code=401,detail="Invalid token")
