32fc7c0eb21566cd7f08e04e7d66b7417657fa9c
Denis Kolesnichenko <dentv2017@gmail.com>
2026-05-26 18:16:55 +0300

    Add files via upload

---
+++ auth_test.py
@@ -0,0 +1,23 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+client = TestClient(app)
+
+
+def test_register_user():
+
+    response = client.post(
+        "/auth/register",
+        json={
+            "email": "test@example.com",
+            "password": "12345678"
+        }
+    )
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data["message"] == "User created successfully"
