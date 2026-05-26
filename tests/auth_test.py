636591a5a0d80e38af003c51cb86134a96de16cb
Denis Kolesnichenko <dentv2017@gmail.com>
2026-05-26 19:14:32 +0300

    Add files via upload

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -42,3 +42,49 @@ def test_register_duplicate_email():
     )
 
     assert response.status_code == 400
+
+
+def test_login_user():
+
+    client.post(
+        "/auth/register",
+        json={
+            "email": "login@example.com",
+            "password": "12345678"
+        }
+    )
+
+    response = client.post(
+        "/auth/login",
+        data={
+            "username": "login@example.com",
+            "password": "12345678"
+        }
+    )
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert "access_token" in data
+
+
+def test_login_wrong_password():
+
+    client.post(
+        "/auth/register",
+        json={
+            "email": "wrongpass@example.com",
+            "password": "12345678"
+        }
+    )
+
+    response = client.post(
+        "/auth/login",
+        data={
+            "username": "wrongpass@example.com",
+            "password": "wrongpassword"
+        }
+    )
+
+    assert response.status_code == 401
