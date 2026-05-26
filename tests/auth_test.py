649e4dc3e3954de6a78a8e4cdd65c5afe32ecf2e
Denis Kolesnichenko <dentv2017@gmail.com>
2026-05-26 18:46:09 +0300

    Add files via upload

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -21,3 +21,24 @@ def test_register_user():
     data = response.json()
 
     assert data["message"] == "User created successfully"
+
+
+def test_register_duplicate_email():
+
+    client.post(
+        "/auth/register",
+        json={
+            "email": "duplicate@example.com",
+            "password": "12345678"
+        }
+    )
+
+    response = client.post(
+        "/auth/register",
+        json={
+            "email": "duplicate@example.com",
+            "password": "12345678"
+        }
+    )
+
+    assert response.status_code == 400
